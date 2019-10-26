fa07ba87bf1c00531b117d056dbae1f4ac647535
albertsomoza <a.somo86@gmail.com>
2019-10-26 20:38:20 +0200

    initial commit

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import typescript from 'rollup-plugin-typescript2';
 import commonjs from 'rollup-plugin-commonjs';
-import copy from 'rollup-plugin-cpy';
 import external from 'rollup-plugin-peer-deps-external';
 import resolve from 'rollup-plugin-node-resolve';
 import url from 'rollup-plugin-url';
@@ -33,32 +32,5 @@ export default {
             exclude: ['*.d.ts', '**/*.d.ts'],
         }),
         commonjs(),
-        copy([
-            // The example uses create-react-app (via create-react-library), which
-            // doesn't work correctly with yarn or npm links. It will end up with
-            // two versions of React in the build, which breaks hooks in particular
-            // since they rely on global state. To avoid this problem we simply copy
-            // the source directly into the example project.
-            //
-            // For more info about the issue:
-            // https://stackoverflow.com/questions/31169760/how-to-avoid-react-loading-twice-with-webpack-when-developing
-            {
-                files: [
-                    'src/index.ts',
-                    'src/provider.tsx',
-                    'src/createStore.ts',
-                    'src/combineReducers.ts',
-                    'src/constants.ts',
-                    'src/applyMiddleware.ts',
-                ],
-                dest: 'example/src/redux-react-hook/',
-            },
-            {
-                files: [
-                    'src/asyncMiddleware/index.ts',
-                ],
-                dest: 'example/src/redux-react-hook/asyncMiddleware',
-            },
-        ]),
     ]
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,2 @@
-export { default as createStore } from './createStore';
-export { default as Provider} from './provider';
-export { default as combineReducers } from './combineReducers';
-export { default as applyMiddleWare } from './applyMiddleware';
-export { default as asyncMiddleware } from './asyncMiddleware';
-
+export {default as flooksThunk} from './middleware';
 
